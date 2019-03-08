dc0a1ba0ad86b9e8c40ee6c1be35d2cebd24fa39
JessieMartinTabilisma <2164631@slu.edu.ph>
2019-03-08 08:00:13 +0800

    Fixing deployment to heroku

--- code/api/app.js
+++ code/api/app.js
@@ -18,10 +18,10 @@ app.use(
 models.sequelize
   .sync()
   .then(result => {
-    app.listen(4000, () => {
+    app.listen(process.env.PORT || 4000, () => {
       console.log('Server is running at port 4000');
     });
   })
   .catch(err => {
-    console.log('Failed to run the hehehasdassasaasdeh: ' + err);
+    console.log('Failed to run the server: ' + err);
   });
