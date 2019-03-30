1b197e878e9ff4634621862b6594799d6de5be95
JessieMartinTabilisma <2164631@slu.edu.ph>
2019-03-31 07:55:11 +0800

    const

--- code/api/app.js
+++ code/api/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+const express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import typeDefs from './graphql/schemas/schema';
 import resolvers from './graphql/resolvers/resolvers';
